dedcc3bd3d73b668855b051ca084136de81a5a2b
miregitgud <ayyash.jibril@gmail.com>
2024-12-20 09:53:40 +0700

    data ingestion research and implementation

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from textSummarizerNLP.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizerNLP.logging import logger
 
-logger.info("Welcome to my world")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========")
+except Exception as e:
+    logger.exception(e)
+    raise e
