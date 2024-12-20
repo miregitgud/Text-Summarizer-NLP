3e1d6d83ef5729a19b1330914daf290765390eea
miregitgud <ayyash.jibril@gmail.com>
2024-12-20 10:21:49 +0700

    data validation research and implementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizerNLP.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizerNLP.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizerNLP.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -6,6 +7,16 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========")
 except Exception as e:
     logger.exception(e)
