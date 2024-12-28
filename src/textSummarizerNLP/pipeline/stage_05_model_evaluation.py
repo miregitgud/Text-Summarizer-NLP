9e30046d1f2b56f524555cf0e594afe29ba463e9
miregitgud <ayyash.jibril@gmail.com>
2024-12-28 23:38:35 +0700

    finalization of implementation base codings

---
+++ src/textSummarizerNLP/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,13 @@
+from textSummarizerNLP.config.configuration import ConfigurationManager
+from textSummarizerNLP.components.model_evaluation import ModelEvaluation
+from textSummarizerNLP.logging import logger
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
